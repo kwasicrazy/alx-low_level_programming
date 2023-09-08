8b5b39fffc28f66a6c43b049d9aa844305406ec7
Seth <sethtsatsu0@gmail.com>
2023-09-07 20:26:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Entry point
- * return 0 (success)
+ *
+ * Return: 0 (success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
-
+	return (0);
 }		
