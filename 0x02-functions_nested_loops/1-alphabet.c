c3a0629a426cf3106f639a14fb621ccd78725cc3
Seth <sethtsatsu0@gmail.com>
2023-09-13 14:56:28 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - function that prints a-z
+ * Return 0;
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' : i <= 'z' : i++)
+		_putchar(i);
+	-putchar('\n');
+}
