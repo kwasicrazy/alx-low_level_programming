a0e230618a8081cd73a6127190e84241d6256dab
Seth <sethtsatsu0@gmail.com>
2023-09-18 06:16:38 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @str: string to be processed
+ */
+void rev_string(char *str)
+{
+	int i, len;
+	char temp;
+
+	len = _strlen(str);
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = str[i];
+		str[i] = str[len - 1 - i];
+		str[len - 1 - i] = temp;
+	}
+}
+
+/**
+ * _strlen - determine the length of the string
+ * @str: string to be processed
+ * Return: length of string
+
