5781c2860d5c0071fa3f69921b1535956afb8175
Seth <sethtsatsu0@gmail.com>
2023-09-18 06:22:00 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -22,4 +22,13 @@ void rev_string(char *str)
  * _strlen - determine the length of the string
  * @str: string to be processed
  * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
 
